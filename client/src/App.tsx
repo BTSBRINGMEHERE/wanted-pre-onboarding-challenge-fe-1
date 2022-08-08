b32e146dea70e3257990d80b1247a0c225a6ce72
movie42 <movie3342@gmail.com>
2022-08-08 22:03:45 +0900

    feat:새로고침을 해도 로그인 유지
    App.tsx
        - 사용자가 새로고침을 하였을 때, 로컬스토리지를 체크하여 로그인 상태라면 로그인을 유지하도록 합니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import useSetUserState from "./lib/hooks/useSetUserState";
 import Routers from "./Routes/Routers";
 
 function App() {
+  const { setUserState } = useSetUserState();
+
+  useEffect(() => {
+    const isUser = localStorage.getItem("justdoit");
+
+    if (isUser) {
+      const { isLogin, token } = JSON.parse(isUser);
+      setUserState({ isLogin, token });
+    }
+  }, []);
+
   return <Routers />;
 }
 

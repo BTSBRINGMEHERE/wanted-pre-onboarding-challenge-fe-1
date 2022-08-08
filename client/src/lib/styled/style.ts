33f1f79581a294da1235cf29c8cc6444c119c67d
movie42 <movie3342@gmail.com>
2022-08-08 22:04:16 +0900

    feat: default style 설정
    GlobalStyle
    - css 초기화
    styled.d.ts
    - styled-components default styled 정의
    style.ts
    - theme
    index
    - theme provider 추가

---
+++ client/src/lib/styled/style.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const style: DefaultTheme = {
+  color: {
+    main: "#FF1E00",
+    second: "#59CE8F",
+    fontMain: "#000000",
+    fontSecond: "#ffffff"
+  }
+};

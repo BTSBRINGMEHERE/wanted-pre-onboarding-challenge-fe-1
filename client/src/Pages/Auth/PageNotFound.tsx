d0b3a1934dcd3da900ae7255625ac72c31a5458d
movie42 <movie3342@gmail.com>
2022-08-10 23:06:24 +0900

    PageNotFound 컴포넌트 수정
    유저가 로그인 상태에서 auth/login, auth/singup 페이지에 접근할 경우 바로 메인 페이지로 리다이렉트 합니다.

--- client/src/Pages/Auth/PageNotFound.tsx
+++ client/src/Pages/Auth/PageNotFound.tsx
@@ -1,20 +1,60 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { userState } from "../../lib/atoms/user";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  h1 {
+    font-size: 5rem;
+    font-weight: bold;
+  }
+`;
 
 interface IPageNotFoundProps {}
 
 const PageNotFound = () => {
+  const params = useParams();
+  const [isAuth, setIsAuth] = useState(false);
   const navigate = useNavigate();
+  const { isLogin } = useRecoilValue(userState);
+
+  const authUrlList = ["auth/login", "auth/signup"];
+
+  const checkAuthUrl = () => {
+    for (let url of authUrlList) {
+      if (url === params["*"]) {
+        setIsAuth(true);
+      }
+    }
+  };
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
+    checkAuthUrl();
 
-    timer = setTimeout(() => navigate("/"), 3000);
+    if (isLogin && isAuth) {
+      navigate("/");
+    }
 
-    return () => clearTimeout(timer);
-  }, []);
+    timer = setTimeout(() => navigate("/"), 2500);
 
-  return <div>페이지를 찾을 수가 없네요</div>;
+    return () => {
+      setIsAuth(false);
+      clearTimeout(timer);
+    };
+  }, [isLogin, isAuth]);
+
+  return (
+    <Wrapper>
+      <h1>🥲 페이지를 찾을 수가 없네요</h1>
+    </Wrapper>
+  );
 };
 
 export default PageNotFound;

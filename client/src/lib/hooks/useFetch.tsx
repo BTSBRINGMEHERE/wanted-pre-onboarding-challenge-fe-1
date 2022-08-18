f1fe2ab0e04a6bd10c8ff2ef4e437c74bfd219ef
movie42 <movie3342@gmail.com>
2022-08-19 02:36:53 +0900

    useFetch axios 적용

--- client/src/lib/hooks/useFetch.tsx
+++ client/src/lib/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { useRecoilValue } from "recoil";
 import { userState } from "../atoms/user";
+import axios from "axios";
 
 const useFetch = <TBody, TData extends unknown>(baseUrl: string) => {
   const { token } = useRecoilValue(userState);
@@ -8,7 +8,8 @@ const useFetch = <TBody, TData extends unknown>(baseUrl: string) => {
   const getData = async (subUrl: string): Promise<TData> => {
     const url = `${baseUrl}${subUrl}`;
 
-    const response = await fetch(url, {
+    const response = await axios({
+      url,
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -16,53 +17,52 @@ const useFetch = <TBody, TData extends unknown>(baseUrl: string) => {
       }
     });
 
-    const { data } = await response.json();
-    return data;
+    return response.data;
   };
 
   const putData = async (subUrl: string, body: TBody): Promise<TData> => {
     const url = `${baseUrl}${subUrl}`;
 
-    const response = await fetch(url, {
+    const response = await axios({
+      url,
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
         Authorization: token
       },
-      body: JSON.stringify(body)
+      data: JSON.stringify(body)
     });
 
-    const { data } = await response.json();
-    return data;
+    return response.data;
   };
 
   const postData = async (subUrl: string, body: TBody): Promise<TData> => {
     const url = `${baseUrl}${subUrl}`;
 
-    const response = await fetch(url, {
+    const response = await axios({
+      url,
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: token
       },
-      body: JSON.stringify(body)
+      data: JSON.stringify(body)
     });
 
-    const { data } = await response.json();
-    return data;
+    return response.data;
   };
 
   const deleteData = async (subUrl: string): Promise<TData> => {
     const url = `${baseUrl}${subUrl}`;
-    const response = await fetch(url, {
+    const response = await axios({
+      url,
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         Authorization: token
       }
     });
-    const { data } = await response.json();
-    return data;
+    return response.data;
   };
 
   return { getData, putData, postData, deleteData };

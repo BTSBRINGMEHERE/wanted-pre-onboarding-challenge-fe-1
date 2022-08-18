d476eb6516405a79d3c4c99e9b541ba19da08d3e
movie42 <movie3342@gmail.com>
2022-08-19 02:31:37 +0900

    snackbar 동작 최상단으로 변경
    main-> App.tsx 로 이동했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useSetUserState } from "./lib/hooks";
+import { useRecoilState } from "recoil";
+import { Snackbar } from "@/Components";
+import { snackbarState } from "@/lib/atoms";
 import { Routers } from "./Routes";
 
 function App() {
   const { setUserState } = useSetUserState();
+  const [snackbarQueue, setSnackbarQueue] = useRecoilState(snackbarState);
 
   useEffect(() => {
     const isUser = localStorage.getItem("justdoit");
@@ -15,8 +19,19 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    timer = setTimeout(() => setSnackbarQueue([]), 6000);
+    return () => clearTimeout(timer);
+  }, [snackbarQueue]);
+
   return (
     <>
+      <Snackbar>
+        {snackbarQueue.map(({ id, message, type }) => (
+          <Snackbar.Item key={id} data-set={id} message={message} type={type} />
+        ))}
+      </Snackbar>
       <ReactQueryDevtools initialIsOpen={false} />
       <Routers />
     </>
--- client/src/Pages/Main/Main.tsx
+++ client/src/Pages/Main/Main.tsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { Snackbar } from "@/Components";
-import { snackbarState, userState } from "@/lib/atoms";
+import { useRecoilValue } from "recoil";
+import { userState } from "@/lib/atoms";
 import { ToDoListContainer } from "@/Pages";
 
 const Main = () => {
   const navigate = useNavigate();
   const { isLogin } = useRecoilValue(userState);
-  const [snackbarQueue, setSnackbarQueue] = useRecoilState(snackbarState);
 
   useEffect(() => {
     if (!isLogin) {
@@ -16,19 +14,8 @@ const Main = () => {
     }
   }, [isLogin]);
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    timer = setTimeout(() => setSnackbarQueue([]), 6000);
-    return () => clearTimeout(timer);
-  }, [snackbarQueue]);
-
   return (
     <>
-      <Snackbar>
-        {snackbarQueue.map(({ id, message, type }) => (
-          <Snackbar.Item key={id} data-set={id} message={message} type={type} />
-        ))}
-      </Snackbar>
       <ToDoListContainer />
     </>
   );

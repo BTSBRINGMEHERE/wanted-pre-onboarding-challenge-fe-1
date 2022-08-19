a8ad88f9ffd6b14a8a13097592a8e0ce71bd9f79
movie42 <movie3342@gmail.com>
2022-08-19 22:45:45 +0900

    회원가입시 이미 존재하는 회원 에러 처리

--- client/src/Pages/Auth/hooks/useSignup.tsx
+++ client/src/Pages/Auth/hooks/useSignup.tsx
@@ -18,33 +18,45 @@ const useSignup = () => {
   const { postData } = useFetch<SignupVariable, SignupData>(mainUrl.baseUrl);
   const setSnackbarQueue = useSetRecoilState(snackbarState);
 
-  return useMutation<SignupData, AxiosError, SignupVariable, unknown>(
-    (body) => postData("/users/create", body),
-    {
-      onSuccess: () => {
+  return useMutation<
+    SignupData,
+    AxiosError<{ data: { detail: string } }>,
+    SignupVariable,
+    unknown
+  >((body) => postData("/users/create", body), {
+    onSuccess: () => {
+      setSnackbarQueue((pre) => [
+        ...pre,
+        {
+          id: Date.now().toString(),
+          message: "✅ 회원가입에 성공했습니다.",
+          type: "notice"
+        }
+      ]);
+    },
+    onError: (error) => {
+      if (error.response?.status === 409) {
         setSnackbarQueue((pre) => [
           ...pre,
           {
             id: Date.now().toString(),
-            message: "✅ 회원가입에 성공했습니다.",
-            type: "notice"
+            message: "⛔️ 이미 존재하는 회원입니다.",
+            type: "warning"
           }
         ]);
-      },
-      onError: (error) => {
-        if (error.response?.status === 400) {
-          setSnackbarQueue((pre) => [
-            ...pre,
-            {
-              id: Date.now().toString(),
-              message: "⛔️ 회원가입에 실패했습니다.",
-              type: "warning"
-            }
-          ]);
-        }
+      }
+      if (error.response?.status === 400) {
+        setSnackbarQueue((pre) => [
+          ...pre,
+          {
+            id: Date.now().toString(),
+            message: "⛔️ 회원가입에 실패했습니다.",
+            type: "warning"
+          }
+        ]);
       }
     }
-  );
+  });
 };
 
 export default useSignup;

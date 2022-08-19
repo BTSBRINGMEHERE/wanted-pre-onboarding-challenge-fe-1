f16bd93bec8d996a7c3231117ef3b54ce4b19be1
movie42 <movie3342@gmail.com>
2022-08-19 21:26:30 +0900

    todo 화면비 제한

--- client/src/Layouts/MainLayout.tsx
+++ client/src/Layouts/MainLayout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "@/Components";
+import styled from "styled-components";
+
+const Main = styled.main`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
     </>
   );
 };
